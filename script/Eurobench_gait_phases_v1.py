b5d95a3dabe597b2f8e5eda6d54b00ef396a3325
Anthony Remazeilles <anthony.remazeilles@tecnalia.com>
2021-03-24 17:08:12 +0100

    initial commit

---
+++ script/Eurobench_gait_phases_v1.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 11:00:13 2021
+
+@author: rschulte
+"""
+
+import yaml
+import numpy as np
+import os
+
+def get_gait_spatiotemporal_params(gait_events):
+    """
+    Parameters
+    ----------
+    gait_events : dict
+        Dictonary containing the gait events for left and right leg, in seconds.
+
+    Returns
+    -------
+    params : dict.
+        Dictonary containing the gait phases of interest: 
+            As fraction of gait cycle for left and right:
+            - Stance phase 
+            - Swing phase
+            - Single Support phase
+            - Double support phase 1
+            - Double support phase 2
+            In seconds for left and right:
+            - Gait cycle
+            In steps per minute:
+            - Cadence
+    """
+     # Initialize params
+    sides = ['l','r']
+    params = {}
+    
+    
+    # Determine single leg gait phases
+    for side in sides:
+        hc = gait_events[side+'_heel_strike']
+        to = gait_events[side+'_toe_off']
+        t_gc = [float(val) for val in np.diff(hc)]
+        params[side+'_t_gait_cycle'] = t_gc
+        if hc[0] < to[0]:
+            # Starts with heel strike
+            params[side+'_t_stance'] = [(to[i]-hc[i])/t_gc[i] for i in range(np.min([len(to),len(hc)]))]
+        else:
+            # Starts with toe-off
+            params[side+'_t_stance'] = [(to[i+1]-hc[i])/t_gc[i] for i in range(np.min([len(to)-1,len(hc)]))]
+        params[side+'_t_swing'] = [1 - val for val in params[side+'_t_stance']]
+    
+    # Double support phases
+    l_hc, l_to = gait_events['l_heel_strike'],gait_events['l_toe_off']
+    r_hc, r_to = gait_events['r_heel_strike'],gait_events['r_toe_off']
+
+    all_gait_events = np.sort(np.concatenate([l_hc,l_to,r_hc,r_to]))
+    ds = {side:[] for side in sides}
+    for i in range(len(all_gait_events)-1):
+        if all_gait_events[i] in l_hc and all_gait_events[i+1] in r_to:
+            ds['l'].append(all_gait_events[i+1]-all_gait_events[i])
+        elif all_gait_events[i] in r_hc and all_gait_events[i+1] in l_to:
+            ds['r'].append(all_gait_events[i+1]-all_gait_events[i])
+    for i_side,side in enumerate(sides):
+        other_side = sides[abs(i_side-1)]
+        t_gc = params[side+'_t_gait_cycle']
+        ds_1 = [ds[side][i]/t_gc[i] for i in range(np.min([len(t_gc),len(ds[side])]))]
+        ds_2 = [ds[other_side][i]/t_gc[i] for i in range(np.min([len(t_gc),len(ds[other_side])]))]
+        params[side+'_t_double_support1'] = ds_1
+        params[side+'_t_double_support2'] = ds_2
+        # single support
+        t_stance = params[side+'_t_stance']
+        params[side+'_t_single_support'] = [t_stance[i]-ds_1[i]-ds_2[i] for i in range(np.min([len(t_stance),len(ds_1),len(ds_2)]))]
+    
+    # Cadence
+    all_hc = np.sort(np.concatenate([l_hc,r_hc])) # steps
+    params['cadence'] = 60/np.diff(all_hc) # steps per minute
+        
+    return params
+
+def print_spatiotemporal_params(params):
+    for key in params:
+        if key.startswith('l') and not 'gait_cycle' in key:
+            print('Left {}: {:.1f} +/- {:.1f}%'.format(key[2:],np.mean(params[key])*100,np.std(params[key])*100))
+        elif key.startswith('r') and not 'gait_cycle' in key:
+            print('Right {}: {:.1f} +/- {:.1f}%'.format(key[2:],np.mean(params[key])*100,np.std(params[key])*100))
+        elif 'cadence' in key:
+            print('Cadence: {:.1f} +/- {:.1f} SPM'.format(np.mean(params[key]),np.std(params[key])))
+
+#%% Load data
+path_saving = 'Data'
+subject = '00'
+cond = '5'
+for file in os.listdir(path_saving):
+    # Check whether it is subject and condition of interest
+    file_subj = file.rsplit('subject_')[-1].rsplit('_')[0]
+    file_cond = file.rsplit('cond_')[-1].rsplit('_')[0]
+    if not (file_subj == subject and file_cond == cond):
+        continue
+    if 'gaitEvents' in file:
+        with open(os.path.join(path_saving,file),'r') as yaml_file:
+            gait_events = yaml.safe_load(yaml_file)
+        # Determine gait phases
+        params = get_gait_spatiotemporal_params(gait_events)
+        print_spatiotemporal_params(params)
+        
---
+++ script/Eurobench_joint_params_v1.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 12:51:22 2021
+
+@author: rschulte
+"""
+
+import yaml
+import numpy as np
+import os
+import pandas as pd
+
+def get_joint_params(joint_data,gait_events):
+    """
+    Parameters
+    ----------
+    joint_data : dict
+        Dictonary containing joint data in degrees and a key 'time' in seconds.
+    gait_events : dict
+        Dictonary containing the gait events for left and right leg, in seconds..
+
+    Returns
+    -------
+    joint_params : dict
+        Dictonary containing joint parameters of interest.
+
+    """
+    joint_params = {}
+    sides = ['l','r'] # More dynamic: np.unique([key.rsplit('_')[0] for key in joint_data if not 'time' in key])
+    joints = [str(val) for val in np.unique([key.rsplit('_')[1] for key in joint_data if not 'time' in key])]
+    time = np.array(joint_data['time'])
+    for side in sides:
+        # Convert gait events from seconds to indices corresponding to time
+        to = np.array([np.argmin(abs(ge-time)) for ge in gait_events[side+'_toe_off']])
+        hc = np.array([np.argmin(abs(ge-time)) for ge in gait_events[side+'_heel_strike']])
+        
+        if hc[0] < to[0]:
+            # Starts with heel strike
+            stance_range = [range(hc[i],to[i]) for i in range(np.min([len(to),len(hc)]))]
+            swing_range = [range(to[i],hc[i+1]) for i in range(np.min([len(to),len(hc)-1]))]
+        else:
+            # Starts with toe off
+            stance_range = [range(hc[i],to[i+1]) for i in range(np.min([len(to)-1,len(hc)]))]
+            swing_range = [range(to[i],hc[i]) for i in range(np.min([len(to),len(hc)]))]
+            
+        for joint in joints:
+            # Only sagital plane (y) of interest
+            angle = np.array(joint_data[side+'_'+joint+'_y'])
+            
+            # Angle at initial contact
+            joint_params[side+'_'+joint+'_ic'] = [angle[i] for i in hc]
+            
+            # Angle at toe off
+            joint_params[side+'_'+joint+'_to'] = [angle[i] for i in to]
+            
+            # Maximal angle during swing
+            joint_params[side+'_'+joint+'_max_swing'] = [angle[r].max() for r in swing_range]
+            
+            # Maximal angle during stance
+            joint_params[side+'_'+joint+'_max_stance'] = [angle[r].max() for r in stance_range]
+
+    return joint_params
+
+def print_joint_params(params):
+    for key in params:
+        if key.startswith('l'):
+            print('Left {}: {:.1f} +/- {:.1f} deg'.format(key[2:],np.mean(params[key]),np.std(params[key])))
+        elif key.startswith('r') and not 'gait_cycle' in key:
+            print('Right {}: {:.1f} +/- {:.1f} deg'.format(key[2:],np.mean(params[key]),np.std(params[key])))
+
+
+#%% Load data
+path_saving = 'Data'
+subject = '00'
+cond = '5'
+run = '00'
+for file in os.listdir(path_saving):
+    # Check whether it is subject and condition of interest
+    file_subj = file.rsplit('subject_')[-1].rsplit('_')[0]
+    file_cond = file.rsplit('cond_')[-1].rsplit('_')[0]
+    file_run = file.rsplit('run_')[-1].rsplit('_')[0]
+    if not (file_subj == subject and file_cond == cond and file_run == run):
+        continue
+    if 'gaitEvents' in file:
+        with open(os.path.join(path_saving,file),'r') as yaml_file:
+            gait_events = yaml.safe_load(yaml_file)
+
+    elif 'jointAngle' in file:
+        joint_data = pd.read_csv(os.path.join(path_saving,file),sep=';').to_dict('list') # Load joint data to dict of lists
+
+# Get joint parameters
+joint_params = get_joint_params(joint_data,gait_events)
+print_joint_params(joint_params)
